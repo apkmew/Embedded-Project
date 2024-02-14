0cc876525a9e9bc643a715f2f1a16449a01ee8ff
mkksdx2 <chaipot.k@ku.th>
2024-02-14 23:39:26 +0700

    add libraries Servo&Ultrasonic and add func changePath&CompareDistance

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,11 @@
 #include <Arduino.h>
+#include <Ultrasonic.h>
+#include <ESP32Servo.h>
+
+Servo servo; // create servo object to control a servo
+Ultrasonic ultrasonic(??); 
+
+long RangeInCentimeters, leftDistance, rightDistance; // variable to store the range in cm
 
 void moveForward();
 void moveBackward();
@@ -15,11 +22,17 @@ void setup() {
   pinMode(18, OUTPUT);  // Left Backward
   pinMode(5, OUTPUT);   // Right Forward
   pinMode(17, OUTPUT);  // Right Backward
-  digitalWrite(2, HIGH);
+  servo.attach(??); // Servo Pin
+  servo.write(90); // Servo Initial Position
 }
 
 void loop() {
-  moveCircle();
+  // put your main code here, to run repeatedly:
+  Serial.println("The distance is: ");
+  RangeInCentimeters = ultrasonic.read();  // Range in cm
+  Serial.print(RangeInCentimeters);
+  Serial.println(" cm");
+  delay(250);
 }
 
 void stop() {
@@ -84,4 +97,36 @@ void moveCircle(){
   delay(1000);
   turnLeft();
   delay(500);
+}
+
+void changePath() {
+  stop(); // stop forward movement
+  moveBackward(); //add back word-aung
+  delay(150);//add move delay-aung
+  stop(); //add stop-aung
+
+  servo.write(??);  // check distance to the right
+  delay(500);
+  rightDistance = ultrasonic.read(); //set right distance
+  delay(500);
+
+  servo.write(??);  // check distance to the left
+  delay(700);
+  leftDistance = ultrasonic.read(); //set left distance
+  delay(500);
+  
+  servo.write(90); // return to center
+  delay(100);
+  compareDistance();
+}
+
+  
+void compareDistance()   // find the longest distance
+{
+  if (leftDistance > rightDistance) { // left is less obstructed 
+    turnLeft();
+  }
+  else if (rightDistance > leftDistance) { // right is less obstructed
+    turnRight();
+  }
 }
